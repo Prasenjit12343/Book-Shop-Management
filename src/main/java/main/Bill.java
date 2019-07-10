c8ce800877625763dd137cbe7a660affe34632fa
PRASENJIT <PRASENJIT@DESKTOP-QS4C7E4>
2019-07-10 12:04:04 +0530

    Book Management project is successfully done.

--- src/main/java/main/Bill.java
+++ src/main/java/main/Bill.java
@@ -8,8 +8,7 @@ public class Bill extends CustomerName{
     //methods like totalAmt ,printBill,discount
     public static String getRandomNumberString() {
         // It wishopTestll generate 6 digit random Number.
-        // from 0 to 999999
-        Random rnd = new Random();
+        // from 0-----d = new Random();
         int number = rnd.nextInt(999999);
     
         // this will convert any number sequence into 6 character.
